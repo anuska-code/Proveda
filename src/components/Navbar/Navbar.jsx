6d51d746ceca1e2348fbaf49e3cc30e4d502345c
anuska-code <anuska.code@gmail.com>
2025-09-15 14:56:31 +0545

    slider added at homepages

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Navbar.module.css";
+import { FaBars, FaTimes } from "react-icons/fa"; // hamburger + close icons
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <>
       {/* Top Support Bar */}
@@ -9,22 +16,24 @@ const Navbar = () => {
         <div className={styles.topContainer}>
           <div className={styles.supportBar}>
             <div className={styles.supportText}>
-              <p>For any assistance call on our toll free number - 1800 103 0214</p>
+              <p>
+                For any assistance call on our toll free number - 1800 103 0214
+              </p>
             </div>
             <div className={styles.socialIcons}>
-              <img 
-                src="/images/insta (1).png" 
-                alt="Instagram" 
+              <img
+                src="/images/insta (1).png"
+                alt="Instagram"
                 className={styles.socialIcon}
               />
-              <img 
-                src="/images/fb.png" 
-                alt="Facebook" 
+              <img
+                src="/images/fb.png"
+                alt="Facebook"
                 className={styles.socialIcon}
               />
-              <img 
-                src="/images/twitter.png" 
-                alt="Twitter" 
+              <img
+                src="/images/twitter.png"
+                alt="Twitter"
                 className={styles.socialIcon}
               />
             </div>
@@ -36,25 +45,45 @@ const Navbar = () => {
       <div className={styles.navbarWrapper}>
         <div className={styles.navbarContainer}>
           <div className={styles.navContent}>
+            {/* Logo Section */}
             <div className={styles.logoSection}>
-              <img 
-                src="/images/provedalogo.png" 
-                alt="Proveda Logo" 
+              <img
+                src="/images/provedalogo.png"
+                alt="Proveda Logo"
                 className={styles.logoImage}
               />
             </div>
 
             {/* Navigation Links */}
-            <div className={styles.navigationLinks}>
-              <a href="/" className={styles.navLink}>Home</a>
-              <a href="/products" className={styles.navLink}>Products</a>
-              <a href="/about" className={styles.navLink}>About Us</a>
+            <div
+              className={`${styles.navigationLinks} ${
+                isMenuOpen ? styles.showMenu : ""
+              }`}
+            >
+              <a href="/" className={styles.navLink}>
+                Home
+              </a>
+              <a href="/products" className={styles.navLink}>
+                Products
+              </a>
+              <a href="/about" className={styles.navLink}>
+                About Us
+              </a>
             </div>
 
             {/* Right Side Actions */}
             <div className={styles.actionSection}>
               <button className={styles.loginButton}>Contact Us</button>
             </div>
+
+            {/* Hamburger Icon (Mobile only) */}
+          <div
+  className={`${styles.hamburger} ${isMenuOpen ? styles.open : ""}`}
+  onClick={toggleMenu}
+>
+  {isMenuOpen ? <FaTimes size={24} color="red" /> : <FaBars size={24} />}
+</div>
+
           </div>
         </div>
       </div>
